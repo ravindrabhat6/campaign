b70a163c1cae4a2cc77934d558e7805b6aea0969
Ravindra <ravindrabhat6@gmail.com>
2018-02-11 00:47:50 +1100

    email confirmation

--- process.php
+++ process.php
@@ -34,12 +34,24 @@ $sql = "INSERT INTO customer2(first_name,last_name,email,genid)
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "Thanks for subscribing. ".$genid."   is your referral code. keep it safe";
+    echo "Thanks for subscribing. ".$genid."   is your referral code. you should be receving email with your Referral code and other information";
 } else {
     echo "Error: info not stored";
 }
 
 $conn->close();
+
+// Subject of confirmation email.
+$conf_subject = 'Your recent enquiry';
+
+// Who should the confirmation email be from?
+$conf_sender = 'MyDept<noreply@mydomain.com>';
+
+$msg = $_POST['first_name'] . ",\n\nWelcome to Telegram.You will be receving our newsletters on a weekly basis.
+Below is your Referral code . please keep it safe.
+\n\n Referfal code: ".$genid."\nPlease contact us if you have any questions";
+
+mail( $_POST['email'], $conf_subject, $msg, 'From: ' . $conf_sender );
 
 
 ?>
