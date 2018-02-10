0d845ae6659d1552cf03869826d78b5af1632d13
Ravindra <ravindrabhat6@gmail.com>
2018-02-11 00:23:58 +1100

    form validation

--- index.php
+++ index.php
@@ -58,13 +58,13 @@
  
 <form method="post" action="process.php">
 	<div>
-		<label for="first_name">First Name:</label> <input type="text" id="first_name" name="first_name">
+		<label for="first_name">First Name:</label> <input type="text" id="first_name" name="first_name" required>
 	</div>
 	<div>
-		<label for="last_name">Last Name:</label> <input type="text" id="last_name" name="last_name">
+		<label for="last_name">Last Name:</label> <input type="text" id="last_name" name="last_name" required>
 	</div>	
 	<div>
-		<label for="email">Email:</label> <input type="email" id="email" name="email">
+		<label for="email">Email:</label> <input type="email" id="email" name="email" required>
 	</div>
  	<div class="button">
           <button type="submit">Submit </button>
